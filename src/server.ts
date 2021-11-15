c1911e01904cfbba52f863918c4febfb5d4f3c7d
Matheus Faria de Carvalho <matheusfariadecarvalho@gmail.com>
2021-11-15 18:24:15 -0300

    Criação de categorys

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const PORT = 3333;
 const app = express();
 app.use(express.json());
+
+app.use("/categories", categoriesRoutes);
 
 app.listen(PORT, () => {
   console.log(
