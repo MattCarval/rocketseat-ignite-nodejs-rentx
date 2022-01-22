446aa843a91844966ec3404cf5a2e8925959ce3c
Matheus Faria de Carvalho <matheusfariadecarvalho@gmail.com>
2022-01-22 02:03:52 -0300

    Start with swagger

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,27 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
+
 import { router } from "./routes";
 
-const PORT = 3333;
 const app = express();
-app.use(express.json());
+const PORT = 3333;
+const SWAGGER_SERVER = "api-docs";
+const serverInformations = [
+  `\n🚀 Server is running! 🚀`,
+  `🚀 Check Swagger Documentation in http://localhost:${PORT}/${SWAGGER_SERVER}/ 🚀`,
+  `🚀 Check server in http://localhost:${PORT} 🚀`,
+];
 
+app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
 app.listen(PORT, () => {
-  console.log(
-    `\n🚀 Server is running! 🚀\n🚀 Check server in http://localhost:${PORT} 🚀\n`
-  );
+  serverInformations.map((information, index) => {
+    if (index !== 0) {
+      return console.log(`${information}`);
+    }
+    return console.log(`\n${information}`);
+  });
 });
