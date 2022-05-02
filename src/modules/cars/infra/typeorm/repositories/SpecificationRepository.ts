3d0ea3bdaef1a188e16c4ab9e35447aaa96319bc
Matheus Faria de Carvalho <matheusfariadecarvalho@gmail.com>
2022-05-02 19:54:23 -0300

    Refatorando estrutura de pastas

--- src/modules/cars/infra/typeorm/repositories/SpecificationRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/SpecificationRepository.ts
@@ -1,9 +1,9 @@
-import { getRepository, Repository } from "typeorm";
-import { Specification } from "../../entities/Specification";
 import {
   ICreateSpecifitaionDTO,
   ISpecificationsRepository,
-} from "../ISpecificationsRepository";
+} from "@modules/cars/repositories/ISpecificationsRepository";
+import { getRepository, Repository } from "typeorm";
+import { Specification } from "../entities/Specification";
 
 class SpecificationsRepository implements ISpecificationsRepository {
   private repository: Repository<Specification>;
--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -3,9 +3,9 @@ import multer from "multer";
 
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
-import { ensureAuthenticated } from "middlewars/ensureAuthenticated";
+import { ensureAuthenticated } from "@shared/infra/http/middlewars/ensureAuthenticated";
 
-import uploadConfig from "../config/upload";
+import uploadConfig from "@config/upload";
 
 const usersRouter = Router();
 const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
